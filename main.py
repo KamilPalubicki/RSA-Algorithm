e0180ad04188013caca5b600b5b717d68c239770
kamil.palubicki <palubicki.kamil@gmail.com>
2021-04-29 00:35:20 +0200

    Finding evaluation of d, public key and private key

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-    def NWD(e, r):
-        while r != 0:
-            e, r = r, e % r
-        return e
+def NWD(e, et):
+    while et != 0:
+        e, et = et, e % et
+    return e
 
-    if __name__ == '__main__':
 
-        p = 13
-        q = 97
+def privateKey(e, et):
+    for d in range(1, 5000):
+        if ((d * e) % et == 1):
+            return d
 
-        n = p * q
-        print("N = ", n)
+if __name__ == '__main__':
 
-        r = (p - 1) * (q - 1)
-        print("tocjent liczby N = ", r)
+    p = 13
+    q = 97
 
-        for i in range(q, 150):
-            if (NWD(i, r) == 1):
-                e = i
-                if e > q:
-                    print("e = ", e)
-                    break
+    n = p * q
+    print("N = ", n)
+
+    et = (p - 1) * (q - 1)
+    print("euler's tocent of N = ", et)
+
+    for i in range(q, 150):
+        if (NWD(i, et) == 1):
+            e = i
+            if e > q:
+                print("e = ", e)
+                break
+    d = privateKey(e, et)
+    print("d = ", d)
+
+    public = (e, n)
+    private = (d, n)
+    print("Public key is", public)
+    print("Private key is:", private)
+
